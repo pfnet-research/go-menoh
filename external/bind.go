c7bc08ca1dcedc63a21e244221e0e4bb2a97aca1
TANAKA Daisuke <duaipp@gmail.com>
2018-06-27 16:02:25 +0900

    modify and sour out bind style

--- external/bind.go
+++ external/bind.go
@@ -1,116 +1,95 @@
-package gomenoh
+package external
 
 /*
 #cgo LDFLAGS: -lmenoh
+
+#include <stdlib.h>
 #include <menoh/menoh.h>
 */
 import "C"
-
 import (
+	"errors"
 	"fmt"
 	"unsafe"
 )
-
-const (
-	_                             = iota
-	Success                       = C.menoh_error_code_success
-	StdError                      = C.menoh_error_code_std_error
-	UnkownError                   = C.menoh_error_code_unknown_error
-	InvalidFilename               = C.menoh_error_code_invalid_filename
-	UnsupportedOnnxOpsetVersion   = C.menoh_error_code_unsupported_onnx_opset_version
-	OnnxParseError                = C.menoh_error_code_onnx_parse_error
-	InvalidDtype                  = C.menoh_error_code_invalid_dtype
-	InvalidAttributeType          = C.menoh_error_code_invalid_attribute_type
-	UnsupportedOperatorAttribute  = C.menoh_error_code_unsupported_operator_attribute
-	DimensionMismatch             = C.menoh_error_code_dimension_mismatch
-	VariableNotFound              = C.menoh_error_code_variable_not_found
-	IndexOutOfRange               = C.menoh_error_code_index_out_of_range
-	UnsupportedOperator           = C.menoh_error_code_unsupported_operator
-	FailedToConfigureOperator     = C.menoh_error_code_failed_to_configure_operator
-	BackendError                  = C.menoh_error_code_backend_error
-	SameNamedVariableAlreadyExist = C.menoh_error_code_same_named_variable_already_exist
-	InvalidDimsSize               = iota
-)
-
-type dType C.menoh_dtype
-
-const (
-	_           = iota
-	Float dType = C.menoh_dtype_float
-)
-
-func errorCheck(code C.menoh_error_code) error {
-	if code != C.menoh_error_code_success {
-		return fmt.Errorf("%s", C.GoString(C.menoh_get_last_error_message()))
-	}
-	return nil
-}
 
 type ModelData struct {
 	h C.menoh_model_data_handle
 }
 
-func MakeModelDataFromOnnx(onnxFileName string) (*ModelData, error) {
+func (m *ModelData) Delete() {
+	C.menoh_delete_model_data(m.h)
+	m.h = nil
+}
+
+func MakeModelDataFromONNX(path string) (*ModelData, error) {
+	cPath := C.CString(path)
+	defer C.free(unsafe.Pointer(cPath))
 	var h C.menoh_model_data_handle
-	if err := errorCheck(C.menoh_make_model_data_from_onnx(C.CString(onnxFileName), &h)); err != nil {
+	if err := checkError(C.menoh_make_model_data_from_onnx(cPath, &h)); err != nil {
 		return nil, err
 	}
 	return &ModelData{h: h}, nil
 }
 
-func (m *ModelData) Delete() {
-	C.menoh_delete_model_data(m.h)
-}
-
-func (m *ModelData) Optimiza(vpt VariableProfileTable) error {
-	return errorCheck(C.menoh_model_data_optimize(m.h, vpt.h))
+func (m *ModelData) Optimize(table VariableProfileTable) error {
+	return checkError(C.menoh_model_data_optimize(m.h, table.h))
 }
 
 type VariableProfileTableBuilder struct {
 	h C.menoh_variable_profile_table_builder_handle
 }
 
+func (b *VariableProfileTableBuilder) Delete() {
+	C.menoh_delete_variable_profile_table_builder(b.h)
+	b.h = nil
+}
+
 func MakeVariableProfileTableBuilder() (*VariableProfileTableBuilder, error) {
 	var h C.menoh_variable_profile_table_builder_handle
-	if err := errorCheck(C.menoh_make_variable_profile_table_builder(&h)); err != nil {
+	if err := checkError(C.menoh_make_variable_profile_table_builder(&h)); err != nil {
 		return nil, err
 	}
 	return &VariableProfileTableBuilder{h: h}, nil
 }
 
-func (v *VariableProfileTableBuilder) Delete() {
-	C.menoh_delete_variable_profile_table_builder(v.h)
-}
-
-func (v *VariableProfileTableBuilder) AddInputProfile(variableName string, dtype dType, dims []int32) error {
-	name := C.CString(variableName)
-	d := C.menoh_dtype(dtype)
-	var err error
-	switch len(dims) {
+func (b *VariableProfileTableBuilder) AddInputProfile(name string, dtype TypeMenohDtype, dims ...int32) error {
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+	switch dimLen := len(dims); dimLen {
 	case 2:
-		err = errorCheck(C.menoh_variable_profile_table_builder_add_input_profile_dims_2(v.h, name, d, C.int32_t(dims[0]), C.int32_t(dims[1])))
+		return checkError(
+			C.menoh_variable_profile_table_builder_add_input_profile_dims_2(
+				b.h, cName, C.int(dtype), C.int(dims[0]), C.int(dims[1])))
 	case 4:
-		err = errorCheck(C.menoh_variable_profile_table_builder_add_input_profile_dims_4(v.h, name, d, C.int32_t(dims[0]), C.int32_t(dims[1]), C.int32_t(dims[2]), C.int32_t(dims[3])))
+		return checkError(
+			C.menoh_variable_profile_table_builder_add_input_profile_dims_4(
+				b.h, cName, C.int(dtype), C.int(dims[0]), C.int(dims[1]), C.int(dims[2]), C.int(dims[3])))
 	default:
-		err = fmt.Errorf("menoh invalid dims size error (2 or 4 is valid): dims size of %s is specified %d", name, len(dims))
+		return fmt.Errorf("dimension size %d is not supported", dimLen)
 	}
-	return err
 }
 
-func (v *VariableProfileTableBuilder) AddOutputProfile(name string, dtype dType) error {
-	return errorCheck(C.menoh_variable_profile_table_builder_add_output_profile(v.h, C.CString(name), C.menoh_dtype(dtype)))
+func (b *VariableProfileTableBuilder) AddOutputProfile(name string, dtype TypeMenohDtype) error {
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+	return checkError(
+		C.menoh_variable_profile_table_builder_add_output_profile(
+			b.h, cName, C.int(dtype)))
 }
 
-func (v *VariableProfileTableBuilder) BuildVariableProfileTable(modelData ModelData) (*VariableProfileTable, error) {
+func (b *VariableProfileTableBuilder) BuildVariableProfileTable(md ModelData) (
+	*VariableProfileTable, error) {
+
 	var h C.menoh_variable_profile_table_handle
-	if err := errorCheck(C.menoh_build_variable_profile_table(v.h, modelData.h, &h)); err != nil {
+	if err := checkError(C.menoh_build_variable_profile_table(b.h, md.h, &h)); err != nil {
 		return nil, err
 	}
 	return &VariableProfileTable{h: h}, nil
 }
 
 type VariableProfile struct {
-	Dtype dType
+	Dtype TypeMenohDtype
 	Dims  []int32
 }
 
@@ -118,98 +97,215 @@ type VariableProfileTable struct {
 	h C.menoh_variable_profile_table_handle
 }
 
-func (v *VariableProfileTable) GetVariableProfile(variableName string) (*VariableProfile, error) {
-	name := C.CString(variableName)
-	var dtype C.menoh_dtype
-	if err := errorCheck(C.menoh_variable_profile_table_get_dtype(v.h, name, &dtype)); err != nil {
+func (t *VariableProfileTable) Delete() {
+	C.menoh_delete_variable_profile_table(t.h)
+	t.h = nil
+}
+
+func (t *VariableProfileTable) GetVariableProfile(name string) (*VariableProfile, error) {
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+	var dtype C.int
+	if err := checkError(C.menoh_variable_profile_table_get_dtype(t.h, cName, &dtype)); err != nil {
 		return nil, err
 	}
-	var dimsSize C.int32_t
-	if err := errorCheck(C.menoh_variable_profile_table_get_dims_size(v.h, name, &dimsSize)); err != nil {
+	var size C.int
+	if err := checkError(C.menoh_variable_profile_table_get_dims_size(t.h, cName, &size)); err != nil {
 		return nil, err
 	}
-	dims := make([]int32, dimsSize)
-	for i := C.int32_t(0); i < dimsSize; i++ {
-		var dim C.int32_t
-		if err := errorCheck(C.menoh_variable_profile_table_get_dims_at(v.h, name, i, &dim)); err != nil {
+	dims := make([]int32, size)
+	for i := 0; i < int(size); i++ {
+		var dim C.int
+		if err := checkError(C.menoh_variable_profile_table_get_dims_at(t.h, cName, C.int(i), &dim)); err != nil {
 			return nil, err
 		}
 		dims[i] = int32(dim)
 	}
-	return &VariableProfile{Dtype: dType(dtype), Dims: dims}, nil
+	return &VariableProfile{
+		Dtype: toDtype(dtype),
+		Dims:  dims,
+	}, nil
 }
 
 type ModelBuilder struct {
 	h C.menoh_model_builder_handle
 }
 
-func MakeModelBuilder(v VariableProfileTable) (*ModelBuilder, error) {
+func (b *ModelBuilder) Delete() {
+	C.menoh_delete_model_builder(b.h)
+	b.h = nil
+}
+
+func MakeModelBuilder(vpt VariableProfileTable) (*ModelBuilder, error) {
 	var h C.menoh_model_builder_handle
-	if err := errorCheck(C.menoh_make_model_builder(v.h, &h)); err != nil {
+	if err := checkError(C.menoh_make_model_builder(vpt.h, &h)); err != nil {
 		return nil, err
 	}
 	return &ModelBuilder{h: h}, nil
 }
 
-func (m *ModelBuilder) Delete() {
-	C.menoh_delete_model_builder(m.h)
+func (b *ModelBuilder) AttachExternalBuffer(variableName string, bufferPtr unsafe.Pointer) error {
+	cVariableName := C.CString(variableName)
+	defer C.free(unsafe.Pointer(cVariableName))
+	return checkError(
+		C.menoh_model_builder_attach_external_buffer(b.h, cVariableName, bufferPtr))
 }
 
-func (m *ModelBuilder) AttachExternalBuffer(name string, buffer unsafe.Pointer) error {
-	return errorCheck(C.menoh_model_builder_attach_external_buffer(m.h, C.CString(name), buffer))
-}
-
-func (m *ModelBuilder) BuildModel(d ModelData, backendName, backendConfig string) (*Model, error) {
+func (b *ModelBuilder) BuildModel(md ModelData, backend, backendConfig string) (*Model, error) {
+	cBackend := C.CString(backend)
+	defer C.free(unsafe.Pointer(cBackend))
+	cBackendConfig := C.CString(backendConfig)
+	defer C.free(unsafe.Pointer(cBackendConfig))
 	var h C.menoh_model_handle
-	if err := errorCheck(C.menoh_build_model(m.h, d.h, C.CString(backendName), C.CString(backendConfig), &h)); err != nil {
+	if err := checkError(C.menoh_build_model(b.h, md.h, cBackend, cBackendConfig, &h)); err != nil {
 		return nil, err
 	}
 	return &Model{h: h}, nil
+}
+
+type Variable struct {
+	Dtype  TypeMenohDtype
+	Dims   []int32
+	BufferHandle unsafe.Pointer
 }
 
 type Model struct {
 	h C.menoh_model_handle
 }
 
-type Variable struct {
-	Dtype        dType
-	Dims         []int32
-	BufferHandle unsafe.Pointer
-}
-
 func (m *Model) Delete() {
 	C.menoh_delete_model(m.h)
+	m.h = nil
 }
 
-func (m *Model) GetVariable(variableName string) (*Variable, error) {
-	name := C.CString(variableName)
-	var buff unsafe.Pointer
-	if err := errorCheck(C.menoh_model_get_variable_buffer_handle(m.h, name, &buff)); err != nil {
+func (m *Model) GetVariable(name string) (*Variable, error) {
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+
+	var dtype C.int
+	e := C.menoh_model_get_variable_dtype(m.h, cName, &dtype)
+	if err := checkError(e); err != nil {
 		return nil, err
 	}
-
-	var dtype C.menoh_dtype
-	if err := errorCheck(C.menoh_model_get_variable_dtype(m.h, name, &dtype)); err != nil {
+	var size C.int
+	e = C.menoh_model_get_variable_dims_size(m.h, cName, &size)
+	if err := checkError(e); err != nil {
 		return nil, err
 	}
-
-	var dimsSize C.int32_t
-	if err := errorCheck(C.menoh_model_get_variable_dims_size(m.h, name, &dimsSize)); err != nil {
-		return nil, err
-	}
-
-	dims := make([]int32, dimsSize)
-	for i := C.int32_t(0); i < dimsSize; i++ {
-		var dim C.int32_t
-		if err := errorCheck(C.menoh_model_get_variable_dims_at(m.h, name, i, &dim)); err != nil {
+	dims := make([]int32, size)
+	for i := 0; i < int(size); i++ {
+		var dim C.int
+		e := C.menoh_model_get_variable_dims_at(m.h, cName, C.int(i), &dim)
+		if err := checkError(e); err != nil {
 			return nil, err
 		}
 		dims[i] = int32(dim)
 	}
+	var ptr unsafe.Pointer
+	e = C.menoh_model_get_variable_buffer_handle(m.h, cName, &ptr)
+	if err := checkError(e); err != nil {
+		return nil, err
+	}
 
-	return &Variable{Dtype: dType(dtype), Dims: dims, BufferHandle: buff}, nil
+	return &Variable{
+		Dtype:  toDtype(dtype),
+		Dims:   dims,
+		BufferHandle: ptr,
+	}, nil
 }
 
 func (m *Model) Run() error {
-	return errorCheck(C.menoh_model_run(m.h))
+	return checkError(C.menoh_model_run(m.h))
+}
+
+// TypeMenohDtype binds 'menoh_dtype_constant' enum
+type TypeMenohDtype C.menoh_dtype
+
+// Dtype
+const (
+	TypeFloat TypeMenohDtype = iota
+)
+
+func toDtype(typeCode C.int) TypeMenohDtype {
+	return TypeMenohDtype(int(typeCode))
+}
+
+type typeMenohError C.menoh_error_code
+
+// Menoh Error type
+const (
+	typeSuccess typeMenohError = iota
+	typeSTDError
+	typeUnknownError
+	typeInvalidFilename
+	typeUnsupportedONNXOpsetVersion
+	typeONNXParseError
+	typeInvalidDtype
+	typeInvalidAttributeType
+	typeUnsupportedOperatorAttribute
+	typeDimensionMismatch
+	typeVariableNotFound
+	typeIndexOutOfRange
+	typeJSONParseError
+	typeInvalidBackendName
+	typeUnsupportedOperator
+	typeFailedToConfigureOperator
+	typeBackendError
+	typeSameNameVariableAlreadyExist
+)
+
+func (e typeMenohError) String() string {
+	switch e {
+	case typeSuccess:
+		return "success"
+	case typeSTDError:
+		return "std error"
+	case typeUnknownError:
+		return "unknown error"
+	case typeInvalidFilename:
+		return "invalid filename"
+	case typeUnsupportedONNXOpsetVersion:
+		return "unsupported ONNX opset version"
+	case typeONNXParseError:
+		return "ONNX parse error"
+	case typeInvalidDtype:
+		return "invalid dtype"
+	case typeInvalidAttributeType:
+		return "invalid attribute type"
+	case typeUnsupportedOperatorAttribute:
+		return "unsupported operator attribute"
+	case typeDimensionMismatch:
+		return "dimension mismatch"
+	case typeVariableNotFound:
+		return "variable not found"
+	case typeIndexOutOfRange:
+		return "index out of range"
+	case typeJSONParseError:
+		return "JSON parse error"
+	case typeInvalidBackendName:
+		return "invalid backend name"
+	case typeUnsupportedOperator:
+		return "unsupported operator"
+	case typeFailedToConfigureOperator:
+		return "failed to configure operator"
+	case typeBackendError:
+		return "backend error"
+	case typeSameNameVariableAlreadyExist:
+		return "save name variable already exist"
+	default:
+		return "unknown type error"
+	}
+}
+
+func checkError(errCode C.int) error {
+	errType := typeMenohError(int(errCode))
+	if errType == typeSuccess {
+		return nil
+	}
+	lastMessage := C.GoString(C.menoh_get_last_error_message())
+	if lastMessage == "" {
+		return errors.New(errType.String())
+	}
+	// last message includes error type
+	return errors.New(lastMessage)
 }
